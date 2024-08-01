0d67fce65252a4cc6f54471301793f694a3dae18
riarobbania <riarobbania@gmail.com>
2024-08-01 16:45:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll(".image-slider img");
+    let currentSlide = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.classList.toggle("active", i === index);
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    setInterval(nextSlide, 3000); // Change slide every 3 seconds
+    showSlide(currentSlide);
+});
